15c49f2f7d82e1ade4e49e2b5e9d96ee68d1b0cd
Axel Jesús Pantoja Romo <axelpantoja08@hotmail.com>
2017-06-29 20:25:36 -0500

    Add project files.

---
+++ Lab09/MainActivity.cs
@@ -0,0 +1,46 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Lab09
+{
+    [Activity(Label = "Lab09", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        TextView tvName;
+        TextView tvStatus;
+        TextView tvToken;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            //Inicializar UI:
+            tvName = FindViewById<TextView>(Resource.Id.tvName);
+            tvStatus = FindViewById<TextView>(Resource.Id.tvStatus);
+            tvToken = FindViewById<TextView>(Resource.Id.tvToken);
+
+            //Validar:
+            string email = "";
+            string password = "";
+            string device = Android.Provider.Settings.Secure.GetString(ContentResolver,
+                Android.Provider.Settings.Secure.AndroidId);
+
+            Validate(email, password, device);
+        }      
+
+        public async void Validate(string Email, string Password, string Device)
+        {
+            var ServiceClient = new SALLab09.ServiceClient();
+            var response = await ServiceClient.ValidateAsync(Email, Password, Device);
+
+            tvName.Text = response.Fullname;
+            tvStatus.Text = response.Status.ToString();
+            tvToken.Text = response.Token;
+        }
+    }
+}
+
